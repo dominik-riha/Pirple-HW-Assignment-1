e87b70a44dc88ff7c93803e590b1d598d3089304
Dominik Riha <dominik.riha@gmail.com>
2018-11-23 00:59:11 +0100

    Get rid of https config

--- config.js
+++ config.js
@@ -2,13 +2,11 @@ var environments = {};
 
 environments.staging = {
 	httpPort: 3000,
-	httpsPort: 3001,
 	envName: 'staging'
 };
 
 environments.production = {
 	httpPort: 5000,
-	httpsPort: 5001,
 	envName: 'production'
 };
 
